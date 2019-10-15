e2e0975c95d4364a0c70899e736d1a43bf17afe8
Louis Irwin <coding@louisirwin.co.uk>
2019-10-15 23:00:29 +0100

    🎉 Initial Commit

---
+++ main.py
@@ -0,0 +1,92 @@
+import socketserver
+from threading import Thread
+
+import websocket, json
+
+connections = {}
+locks = {}
+
+class service(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        self.data = self.request.recv(1024).strip().decode("utf-8")
+        command = self.data.split("\n")
+
+        print(command)
+
+        if command[0] == "connect":
+            result = self.connect(command[1:])
+        elif command[0] == "connect_test":
+            result = self.connect_test(command[1:])
+        elif command[0] == "send_to_device":
+            result = self.send_to_device(command[1:])
+
+        self.request.sendall(result)
+
+    # args: [id, ip]
+    def connect(self, args):
+        if args[0] not in connections:
+            try:
+                connections[args[0]] = websocket.WebSocket()
+                connections[args[0]].connect("ws://" + args[1] + ":8266")
+                connections[args[0]].recv()
+                connections[args[0]].send("secret\n")
+                connections[args[0]].recv()
+                connections[args[0]].send("import user, json\r\n")
+                for x in range(0,len("import user, json") + 2):
+                    connections[args[0]].recv()
+                locks[args[0]] = False
+                return b'true'
+            except Exception as e:
+                if args[0] in connections:
+                    del connections[args[0]]
+                return(b'false\n' + str(e).encode())
+        else:
+            return b'false\nDevice already connected'
+
+    # args: [id]
+    def connect_test(self, args):
+        if args[0] in connections:
+            return b'true'
+        else:
+            return b'false'
+
+    # args: [id, command, args]
+    def send_to_device(self, args):
+        args[2] = json.loads(args[2])
+        arg_string = ""
+        for arg in args[2]:
+            arg_string += str(arg) + ","
+        command = "print(json.dumps(user." + args[1] + "(" + arg_string + ")))"
+
+        while locks[args[0]]:
+            pass
+        locks[args[0]] = True
+
+        ws = connections[args[0]]
+
+        ws.send(command + "\r\n")
+
+        result = ""
+        addition = ""
+        while addition != ">>> ":
+            addition = ws.recv()
+            result += addition if addition != ">>> " else ""
+
+        locks[args[0]] = False
+
+        result_start = result.index("\r\n") + 2
+        result = result[result_start:]
+
+        return result.encode()
+
+class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
+    pass
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 1102
+    server = ThreadedTCPServer((HOST, PORT), service)
+    try:
+        server.serve_forever()
+    except Exception as e:
+        server.shutdown()
