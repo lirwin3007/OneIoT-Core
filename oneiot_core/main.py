c845ba1ef7c6b159b5b59fb98ae318148ccab756
Louis Irwin <coding@louisirwin.co.uk>
2020-01-27 20:17:46 +0000

    ♻️ Transitioned back to web server approach

--- oneiot_core/main.py
+++ oneiot_core/main.py
@@ -1,80 +1,84 @@
 import socketserver
 from threading import Thread
 
+import requests
+
 import webrepl_cli
 import websocket, json
 
-connections = {}
-locks = {}
+# connections = {}
+# locks = {}
 
 class service(socketserver.BaseRequestHandler):
 
     def handle(self):
         self.data = self.request.recv(1024).strip().decode("utf-8")
         command = self.data.split("\n")
+        result = ""
 
         print(command)
 
-        if command[0] == "connect":
-            result = self.connect(command[1:])
-        elif command[0] == "connect_test":
+        # if command[0] == "connect":
+        #     result = self.connect(command[1:])
+        if command[0] == "connect_test":
             result = self.connect_test(command[1:])
         elif command[0] == "send_to_device":
             result = self.send_to_device(command[1:])
         elif command[0] == "reset":
             result = self.reset(command[1:])
-        elif command[0] == "disconnect":
-            result = self.disconnect(command[1:])
+        # elif command[0] == "disconnect":
+        #     result = self.disconnect(command[1:])
         elif command[0] == "upload":
             result = self.upload(command[1:])
 
         self.request.sendall(result)
 
     # args: [id, ip]
-    def connect(self, args):
-        if args[0] not in connections:
-            try:
-                connections[args[0]] = websocket.WebSocket()
-                connections[args[0]].connect("ws://" + args[1] + ":8266")
-                connections[args[0]].recv()
-                connections[args[0]].send("secret\n")
-                connections[args[0]].recv()
-                connections[args[0]].send("import user, json\r\n")
-                for x in range(0,len("import user, json") + 2):
-                    connections[args[0]].recv()
-                locks[args[0]] = False
-                return b'true'
-            except Exception as e:
-                if args[0] in connections:
-                    del connections[args[0]]
-                return(b'false\n' + str(e).encode())
-        else:
-            return b'false\nDevice already connected'
+    # def connect(self, args):
+    #     if args[0] not in connections:
+    #         try:
+    #             connections[args[0]] = websocket.WebSocket()
+    #             connections[args[0]].connect("ws://" + args[1] + ":8266")
+    #             connections[args[0]].recv()
+    #             connections[args[0]].send("secret\n")
+    #             connections[args[0]].recv()
+    #             connections[args[0]].send("import user, json\r\n")
+    #             for x in range(0,len("import user, json") + 2):
+    #                 connections[args[0]].recv()
+    #             locks[args[0]] = False
+    #             return b'true'
+    #         except Exception as e:
+    #             if args[0] in connections:
+    #                 del connections[args[0]]
+    #             return(b'false\n' + str(e).encode())
+    #     else:
+    #         return b'false\nDevice already connected'
 
     # args: [id, ip]
-    def disconnect(self, args):
-        if args[0] in connections:
-            connections[args[0]].close()
-            del locks[args[0]]
-            del connections[args[0]]
-            return b'true'
-        else:
-            return b'false\nDevice not connected'
+    # def disconnect(self, args):
+    #     if args[0] in connections:
+    #         connections[args[0]].close()
+    #         del locks[args[0]]
+    #         del connections[args[0]]
+    #         return b'true'
+    #     else:
+    #         return b'false\nDevice not connected'
 
     # args: [id, ip]
     def reset(self, args):
-        if args[0] in connections:
-            while locks[args[0]]:
-                pass
-            locks[args[0]] = True
-            connections[args[0]].send("import machine\r\n")
-            connections[args[0]].send("machine.reset()\r\n")
-            self.disconnect([args[0]])
-            self.connect([args[0], args[1]])
-            locks[args[0]] = False
-            return b'true'
-        else:
-            return b'false\nDevice not connected'
+        return b'false\nNot yet implemented'
+        # if args[0] in connections:
+        #     while locks[args[0]]:
+        #         pass
+        #     locks[args[0]] = True
+        #     connections[args[0]].send("import machine\r\n")
+        #     connections[args[0]].send("machine.reset()\r\n")
+        #     self.disconnect([args[0]])
+        #     self.connect([args[0], args[1]])
+        #     locks[args[0]] = False
+        #     return b'true'
+        # else:
+        #     return b'false\nDevice not connected'
 
     # args: [id, ip, source, destination]
     def upload(self, args):
@@ -82,47 +86,50 @@ class service(socketserver.BaseRequestHandler):
         ip = args[1]
         source = args[2]
         destination = args[3]
-        self.disconnect([id])
+        #self.disconnect([id])
         webrepl_cli.main('secret', ip + ':' + destination, 'put', src_file=source)
-        self.connect([id, ip])
+        #self.connect([id, ip])
         self.reset([id, ip])
         return b'true'
 
     # args: [id]
     def connect_test(self, args):
-        if args[0] in connections:
-            return b'true'
-        else:
-            return b'false'
+        return b'true'
+        # if args[0] in connections:
+        #     return b'true'
+        # else:
+        #     return b'false'
 
-    # args: [id, command, args]
+    # args: [id, ip, command, args (string)]
     def send_to_device(self, args):
-        args[2] = json.loads(args[2])
-        arg_string = ""
-        for arg in args[2]:
-            arg_string += str(arg) + ","
-        command = "print(json.dumps(user." + args[1] + "(" + arg_string + ")))"
-
-        while locks[args[0]]:
-            pass
-        locks[args[0]] = True
-
-        ws = connections[args[0]]
-
-        ws.send(command + "\r\n")
-
-        result = ""
-        addition = ""
-        while addition != ">>> ":
-            addition = ws.recv()
-            result += addition if addition != ">>> " else ""
-
-        locks[args[0]] = False
-
-        result_start = result.index("\r\n") + 2
-        result = result[result_start:]
-
-        return result.encode()
+        result = requests.post('http://' + args[1] + '/' + args[2], data=args[3])
+        return result.content
+        # args[2] = json.loads(args[2])
+        # arg_string = ""
+        # for arg in args[2]:
+        #     arg_string += str(arg) + ","
+        # command = "print(json.dumps(user." + args[1] + "(" + arg_string + ")))"
+        #
+        # while locks[args[0]]:
+        #     pass
+        # locks[args[0]] = True
+        #
+        # ws = connections[args[0]]
+        #
+        # ws.send(command + "\r\n")
+        #
+        # result = ""
+        # addition = ""
+        # while addition != ">>> ":
+        #     addition = ws.recv()
+        #     result += addition if addition != ">>> " else ""
+        #
+        # locks[args[0]] = False
+        #
+        # result_start = result.index("\r\n") + 2
+        # result = result[result_start:]
+        #
+        # return result.encode()
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     pass
@@ -133,4 +140,5 @@ if __name__ == "__main__":
     try:
         server.serve_forever()
     except Exception as e:
+        print(e)
         server.shutdown()
